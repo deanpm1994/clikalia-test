dcd5b237de4c7c5000bc361b5f0bf68a45fbcd7c
Dean Pérez Martín <deanpm1994@gmail.com>
2022-04-08 18:27:11 +0200

    Make a simple table

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import axios from 'axios';
 
 const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  const getPokemons = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then(response => {
+        console.log(response.data)
+        setPokemons(response.data.results)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [getPokemons])
+
   return (
     <Container maxWidth="md">
-
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableCell>Nombre</TableCell>
+              <TableCell>URL</TableCell>
+            </TableHead>
+            <TableBody>
+              {pokemons.map((pokemon, index) => (
+                <TableRow
+                  key={index}
+                >
+                  <TableCell>{pokemon.name}</TableCell>
+                  <TableCell>{pokemon.url}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </Container>
   )
 }
