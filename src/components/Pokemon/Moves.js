89f98321bcd617f77a319981a3327958c994b515
Dean Pérez Martín <deanpm1994@gmail.com>
2022-04-09 01:50:27 +0200

    Remove moves

--- src/components/Pokemon/Moves.js
+++ src/components/Pokemon/Moves.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { get, sortBy } from 'lodash'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { get, sortBy, reverse } from 'lodash'
 
 const Moves = ({ pokemon }) => {
+  const [moves, setMoves] = useState([])
+
+  useEffect(() => {
+    if (get(pokemon, 'moves.length')) {
+      setMoves(reverse(sortBy(pokemon.moves, ['move.url'])))
+    }
+  }, [pokemon])
+
   if (!get(pokemon, 'moves.length')) return null
+
+  const deleteMove = index => {
+    let movesCopy = [...moves]
+    movesCopy.splice(index, 1)
+    setMoves(movesCopy)
+  }
 
   return (
     <Accordion>
@@ -20,12 +36,16 @@ const Moves = ({ pokemon }) => {
         <Typography>Moves</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        {sortBy(pokemon.moves, ['move.url'])
-          .reverse()
-          .map((move, index) => (
+        {moves.map((move, index) => (
           <List key={index}>
             <ListItem disablePadding>
-              <ListItemText primary={move.move.name} />
+              <ListItemText primary={move.move.name} secondary={move.move.url}/>
+              <IconButton
+                color="error"
+                onClick={() => deleteMove(index)}
+              >
+                <DeleteIcon />
+              </IconButton>
             </ListItem>
           </List>
         ))}
