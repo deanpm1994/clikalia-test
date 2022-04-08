e33192b16072dacb4247393a178c2013f3d47af4
Dean Pérez Martín <deanpm1994@gmail.com>
2022-04-08 19:04:49 +0200

    Make simple pagination

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Container } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import TableContainer from '@mui/material/TableContainer';
@@ -7,34 +7,66 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([])
+  const [page, setPage] = useState(0)
+  const [count, setCount] = useState(0)
+  const [next, setNext] = useState(0)
+  const [previous, setPrevious] = useState(0)
 
-  const getPokemons = () => {
-    axios.get('https://pokeapi.co/api/v2/pokemon')
-      .then(response => {
-        console.log(response.data)
-        setPokemons(response.data.results)
+  const getPokemons = useCallback(
+    (url) => {
+      axios.get(url
+        ? url
+        : 'https://pokeapi.co/api/v2/pokemon', {
+        params: {
+          limit: 20,
+        }
       })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+        .then(response => {
+          console.log(response.data)
+
+          setPokemons(response.data.results)
+          setNext(response.data.next)
+          setPrevious(response.data.previous)
+          setCount(response.data.count)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    [],
+  )
 
   useEffect(() => {
     getPokemons()
   }, [getPokemons])
 
+  const handleChangePage = (event, pageToChange) => {
+    if (pageToChange > page && next) {
+      getPokemons(next)
+      setPage(pageToChange)
+    }
+    if (pageToChange < page && previous) {
+      getPokemons(previous)
+      setPage(pageToChange)
+    }
+  }
+
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" sx={{ paddingTop: '2.5rem' }}>
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer sx={{ maxHeight: 440 }}>
+        <TableContainer sx={{ maxHeight: '80vh' }}>
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
-              <TableCell>Nombre</TableCell>
-              <TableCell>URL</TableCell>
+              <TableRow>
+                <TableCell>Nombre</TableCell>
+                <TableCell>URL</TableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
               {pokemons.map((pokemon, index) => (
@@ -42,13 +74,26 @@ const PokemonList = () => {
                   key={index}
                 >
                   <TableCell>{pokemon.name}</TableCell>
-                  <TableCell>{pokemon.url}</TableCell>
+                  <TableCell>
+                    <Link to={`pokemon/${pokemon.url}`}>
+                      {pokemon.url}
+                    </Link>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       </Paper>
+      <TablePagination
+        rowsPerPageOptions={[20]}
+        component="div"
+        count={count}
+        rowsPerPage={20}
+        page={page}
+        onPageChange={handleChangePage}
+      // onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </Container>
   )
 }
